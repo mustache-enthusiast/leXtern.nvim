7607facec22dcb54d37c26faacdd4a1da3407520
mustache-enthusiast <cliff29hanger@gmail.com>
2025-10-27 23:37:34 -0600

    Remove superflous LeXternDirTest function definition

--- plugin/lextern.lua
+++ plugin/lextern.lua
@@ -11,18 +11,6 @@ vim.api.nvim_create_user_command('LeXternTest', function(opts)
 end, { nargs = 1 })
 
 
-vim.api.nvim_create_user_command('LeXternTestDir', function()
-  local utils = require('lextern.utils')
-  local dir = utils.get_figures_dir()
-  
-  if dir then
-    print("Figures directory: " .. dir)
-  else
-    print("Could not determine figures directory (no file open)")
-  end
-end, {})
-
-
 -- Test get_figures_dir
 vim.api.nvim_create_user_command('LeXternDir', function()
   local utils = require('lextern.utils')
